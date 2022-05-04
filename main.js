22e48c44d60587fd6044d68e8d733e155cd74a12
allansuzuki <allan.suzuki@outlook.com>
2022-05-03 23:27:43 -0300

    dia2 - nav&menu

---
+++ main.js
@@ -0,0 +1,10 @@
+function onscroll() {
+  // if (scrollY>0) {
+  //   nav.classList.add('scroll')
+  // }
+  // else {
+  //   nav.classList.remove('scroll')
+  // }
+
+  scrollY > 0 ? nav.classList.add('scroll') : nav.classList.remove('scroll')
+}
